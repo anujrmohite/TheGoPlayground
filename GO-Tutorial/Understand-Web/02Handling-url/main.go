f23aba7d4f51e8832eaebfaf80d77abf4d9fdfa5
anujrmohite <anujmohite001@gmail.com>
2023-06-09 17:44:52 +0530

    url

---
+++ GO-Tutorial/Understand-Web/02Handling-url/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj45fd"
+
+func main() {
+	fmt.Println("Handling URL in GOlang")
+	fmt.Println(myurl)
+
+	//parsing URL
+
+	result, err := url.Parse(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	q_params := result.Query()
+	fmt.Println("Types of Params is :")
+	fmt.Println(q_params)
+}
