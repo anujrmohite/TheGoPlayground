fb4fe6a69792b788cd72c3ce0b0624812dae55c4
anujrmohite <anujmohite001@gmail.com>
2023-08-28 19:03:24 +0530

    add

--- A Basic Web Server with golang/main.go
+++ A Basic Web Server with golang/main.go
@@ -1,31 +1,56 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r http.Request)  {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	// ParseForm is necessary to extract form data from the request.
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("ParseForm() error: %v", err), http.StatusInternalServerError)
 		return
 	}
-	if r.Method != "GET" {
-		
+
+	// Handle POST request
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		address := r.FormValue("address")
+		fmt.Fprintln(w, "POST request successful")
+		fmt.Fprintf(w, "Name: %s\n", name)
+		fmt.Fprintf(w, "Address: %s\n", address)
+	} else {
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
 	}
 }
 
-func main()  {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/",fileServer)
-	http.Handle("/form", formHandler)
-	http.Handle("/hello", helloHandler)
-
-	fmt.Println("Starting server at port 8080\n")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil{
-		log.Fatal(err)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.NotFound(w, r) // Use http.NotFound to handle 404 responses.
+		return
 	}
 
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintln(w, "Hello")
+}
+
+func main() {
+	// Serving static files from the "static" directory.
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	// Handling form submissions and hello route.
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server at port 8080")
+
+	// Start the HTTP server.
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
